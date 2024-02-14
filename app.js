6a018b7764dff6c03cfd4c64d03f2ba8de612066
Eduardo Bautista <eduardobautistauribe7@gmail.com>
2024-02-14 15:55:26 -0500

    setup server and define routes

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const express = require('express')
 const app = express();
 
@@ -5,6 +6,19 @@ app.get('/',(req,res)=>{
     res.send('hey')
 })
 
-app.listen(3000,()=>{
-    console.log('listening')
+//2. Define routes in your head:
+/*
+app.get('/api/v1/tasks')        -get all tasks
+app.get('/api/v1/tasks/:id)      -get one task
+app.post('/api/v1/tasks')        -create new task
+app.patch('/api/v1/tasks/:id')   -update task
+app.delete('/api/v1/tasks/:id')  -delete task
+
+*/
+
+const port = 3000
+
+//1. setup server
+app.listen(port,()=>{
+    console.log(`listening on port ${port}`)
 })
