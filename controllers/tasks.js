43a62a2e4f0f81c3e7388689a7b78ad61c28c4d0
Eduardo Bautista <eduardobautistauribe7@gmail.com>
2024-02-14 16:29:12 -0500

    add even more routes and controllers

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -4,6 +4,28 @@ const getAllTasks = (req,res) => {
     res.send('all items from the file')
 }
 
+const createTask = (req,res) => {
+    res.json(req.body)
+}
+
+const getTask = (req,res) => {
+    res.send('get single task')
+}
+
+const updateTask = (req,res) => {
+    res.send('update task')
+}
+
+const deleteTask = (req,res) => {
+    res.send('delete task')
+}
+
+//7. After setting up controllers, set up postman for testing
+//by creating a collection
 module.exports = {
-    getAllTasks
+    getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
 }
